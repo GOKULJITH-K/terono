3689de9d7b3a9271723932390230cd504b5eb532
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-06-01 09:35:07 +0200

    Add files via upload

--- public/navbar.js
+++ public/navbar.js
@@ -1,12 +1,19 @@
-const sidemenu= document.getElementById("sidemenu");
-const navicon= document.getElementById("nav");
-
+const sidemenu= document.getElementById("sidemenu2");
+const navicon= document.getElementById("morebtn");
+var sidemenu2= document.getElementById("sidemenu");
+const sidemenu3= document.getElementById("sidemenu3");
 function openmenu(){
+    sidemenu2.style.right="0";
+}
+function closemenu(){
+    sidemenu2.style.right="-200%";
+}
+function notification(){
     if(sidemenu.style.top=='100%'){
 
     sidemenu.style.bottom="0%";
-    sidemenu.style.top="45%";
-    navicon.style.color="red";
+    sidemenu.style.top="66%";
+    navicon.style.color="black";
     }
 
 else{
@@ -16,3 +23,4 @@ else{
     
 }
  }
+ 
